5dfa751d80aaff3ce942664cf9dfe1dfc0af0977
Manassarn Manoonchai <manassarn@gmail.com>
2021-05-28 01:13:16 +0700

    :sparkles: Extract Thai tweets

--- lib/triads_extractor.ex
+++ lib/triads_extractor.ex
@@ -1,7 +1,19 @@
 defmodule TriadsExtractor do
+  use Application
+
   @moduledoc """
   Documentation for `TriadsExtractor`.
   """
+
+
+  def start(_type, _args) do
+    IO.puts "starting"
+    run()
+    # some more stuff
+
+    # Exit gracefully
+    Supervisor.start_link [], strategy: :one_for_one
+  end
 
   @doc """
   Hello world.
@@ -12,7 +24,17 @@ defmodule TriadsExtractor do
       :world
 
   """
-  def hello do
-    :world
+  def run do
+    output_file = File.stream!("out.txt")
+
+    File.stream!("in.csv")
+    |> CSV.decode(headers: true, separator: ?\t)
+    |> Stream.map(fn({:ok, row}) -> extract_thai_tweet(row) <> "\n" end)
+    |> Stream.filter(fn str -> String.length(str) >= 3 end)
+    |> Enum.into(output_file)
+  end
+
+  def extract_thai_tweet(row) do
+    Regex.scan(~r/[ก-๛]/u, row["tweet"]) |> Enum.join
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule TriadsExtractor.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {TriadsExtractor, []},
       extra_applications: [:logger]
     ]
   end
@@ -21,8 +22,7 @@ defmodule TriadsExtractor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:csv, "~> 2.4"}
     ]
   end
 end

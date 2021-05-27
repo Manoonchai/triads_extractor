0e4aa5ce228c3c70056b52e2bc2eaeeeb4f1ccd4
Manassarn Manoonchai <manassarn@gmail.com>
2021-05-28 02:16:47 +0700

    :sparkles: Stream with inspection

--- lib/triads_extractor.ex
+++ lib/triads_extractor.ex
@@ -29,9 +29,16 @@ defmodule TriadsExtractor do
 
     File.stream!("in.csv")
     |> CSV.decode(headers: true, separator: ?\t)
-    |> Stream.map(fn({:ok, row}) -> extract_thai_tweet(row) <> "\n" end)
+    |> Stream.map(fn({:ok, row}) -> extract_thai_tweet(row) end)
     |> Stream.filter(fn str -> String.length(str) >= 3 end)
-    |> Enum.into(output_file)
+    |> Stream.zip(Stream.iterate(1, &(&1 + 1)))
+    |> Stream.each(fn({str, count}) ->
+      IO.puts(to_string(count) <> " | " <> str)
+    end)
+    |> Stream.map(fn({str, _}) -> str end)
+    |> Stream.intersperse("\n")
+    |> Stream.into(output_file)
+    |> Stream.run
   end
 
   def extract_thai_tweet(row) do

e4c0a6c3d626148598fdea8ca6f24ee896774a26
Manassarn Manoonchai <manassarn@gmail.com>
2021-05-28 23:42:13 +0700

    :sparkles: Extract triads from line

--- lib/triads_extractor.ex
+++ lib/triads_extractor.ex
@@ -3,15 +3,6 @@ defmodule TriadsExtractor do
   Documentation for `TriadsExtractor`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TriadsExtractor.hello()
-      :world
-
-  """
   def run(input, output) do
     output_file = File.stream!(output || "out.txt")
 
@@ -44,5 +35,38 @@ defmodule TriadsExtractor do
     end)
     |> Enum.filter(fn str -> String.length(str) >= 3 end)
     |> Enum.join(" ")
+  end
+
+  @doc """
+  Tallys multiple string separated by space to triads map with frequencies
+
+  ## Examples
+
+      iex> TriadsExtractor.line_to_triads("abcd bcde")
+      %{ "abc" => 1, "bcd" => 2, "cde" => 1 }
+
+  """
+  def line_to_triads(str) do
+    str
+    |> String.split()
+    |> Enum.map(&to_triads(&1))
+    |> Enum.reduce(%{}, fn x, acc -> Map.merge(x, acc, fn _k, a, b -> a + b end) end)
+  end
+
+  @doc """
+  Tallys string to triads map with frequencies
+
+  ## Examples
+
+      iex> TriadsExtractor.to_triads("abcd")
+      %{ "abc" => 1, "bcd" => 1 }
+
+  """
+  def to_triads(str) do
+    str
+    |> String.codepoints()
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.join(&1))
+    |> Enum.frequencies()
   end
 end
--- test/triads_extractor_test.exs
+++ test/triads_extractor_test.exs
@@ -2,7 +2,23 @@ defmodule TriadsExtractorTest do
   use ExUnit.Case
   doctest TriadsExtractor
 
-  test "greets the world" do
-    assert TriadsExtractor.hello() == :world
+  test "to_triads" do
+    assert TriadsExtractor.to_triads("abc") == %{"abc" => 1}
+    assert TriadsExtractor.to_triads("abcabc") == %{"abc" => 2, "bca" => 1, "cab" => 1}
+    assert TriadsExtractor.to_triads("ab") == %{}
+  end
+
+  test "line_to_triads" do
+    assert TriadsExtractor.line_to_triads("abc abc") == %{"abc" => 2}
+    assert TriadsExtractor.line_to_triads("abcabc") == %{"abc" => 2, "bca" => 1, "cab" => 1}
+    assert TriadsExtractor.line_to_triads("ab ab") == %{}
+
+    assert TriadsExtractor.line_to_triads("ที่ทิ้ง") == %{
+             "ที่" => 1,
+             "ี่ท" => 1,
+             "่ทิ" => 1,
+             "ทิ้" => 1,
+             "ิ้ง" => 1
+           }
   end
 end
